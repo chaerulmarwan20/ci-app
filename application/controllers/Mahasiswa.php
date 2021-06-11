fd318436a7192a69505e32191a993d91ddcf7e2a
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-06-11 15:26:06 +0700

    add read

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -4,11 +4,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Mahasiswa extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Mahasiswa_model');
+    }
+
     public function index()
     {
         $data['judul'] = "Daftar Mahasiswa";
+
+        $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
         $this->load->view('templates/header', $data);
-        $this->load->view('mahasiswa/index');
+        $this->load->view('mahasiswa/index', $data);
         $this->load->view('templates/footer');
     }
 }
---
+++ application/models/Mahasiswa_model.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Mahasiswa_model extends CI_Model
+{
+    public function getAllMahasiswa()
+    {
+        return $this->db->get('mahasiswa')->result_array();
+    }
+}
+
+/* End of file Mahasiswa_model.php */
