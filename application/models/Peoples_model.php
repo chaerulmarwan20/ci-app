3666cfeecb6dda4d3eaec7b9b943aab378a4af32
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-06-11 21:38:26 +0700

    add pagination & fix search

---
+++ application/models/Peoples_model.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Peoples_model extends CI_Model
+{
+    public function getAllPeoples()
+    {
+        return $this->db->get('peoples')->result_array();
+    }
+
+    public function getPeoples($limit, $start, $keyword = null)
+    {
+        if ($keyword) {
+            $this->db->like('name', $keyword);
+            $this->db->or_like('email', $keyword);
+        }
+        return $this->db->get('peoples', $limit, $start)->result_array();
+    }
+
+    public function countAllPeoples()
+    {
+        return $this->db->get('peoples')->num_rows();
+    }
+}
+
+/* End of file Peoples_model.php */
